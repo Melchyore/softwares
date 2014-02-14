aa2f25174359582b4c2e2d285c55dafcd6acf2fb
Wisleur <rider-2000@hotmail.com>
2014-02-14 23:00:39 +0100

    Add built Version.

---
+++ Version-build-desktop/release/ver.h
@@ -0,0 +1,17 @@
+#include <windows.h>
+FILEVERSION         2, 3, 0, 20045
+PRODUCTVERSION      2, 3, 0, 20045
+FILEFLAGSMASK       0x3fL
+FILEFLAGS           0
+FILETYPE            VFT_APP
+FILESUBTYPE         VFT2_UNKNOWN
+BEGIN
+    BLOCK   "VarFileInfo"
+    BEGIN
+        VALUE   "Translation",  0x409,  1200
+    END
+    BLOCK   "StringFileInfo"
+    BEGIN
+        BLOCK   "040904b0"
+        BEGIN
+            VALUE   "CompanyName",      "Test
